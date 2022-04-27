22e2e8f7598274efe3ecbee1178bdb33efffd49f
heuserhunter0 <87386401+heuserhunter0@users.noreply.github.com>
2022-04-26 19:10:22 -0600

    finished the game

--- CSE210_proveassignments/tictactoe.py
+++ CSE210_proveassignments/tictactoe.py
@@ -3,7 +3,14 @@
 
 # runs program
 def main():
-   print() 
+    player = next_player("")
+    board = game_board()
+    while not (has_winner(board) or match_draw(board)):
+        display_board(board)
+        player_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print()
 
 # the game board
 def game_board():
@@ -22,3 +29,37 @@ def display_board(board):
     print(f"{board[6]}|{board[7]}|{board[8]}")
     # space between the board and terminal
     print()
+
+# determine if the match is a draw
+def match_draw(board):
+    for tile in range(9):
+        if board[tile] != "x" and board[tile] != "o":
+            return False
+    return True 
+
+# check for winner
+def has_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
+
+# allow a player to choose a tile
+def player_move(player, board):
+    tile = int(input(f"{player}'s turn to choose a sqaure (1-9): "))
+    board[tile - 1] = player
+
+# next player's turn
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+# run main function
+if __name__ == "__main__":
+    main()
